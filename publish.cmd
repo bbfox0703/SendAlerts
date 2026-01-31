2145cec1cb6979cce13559be89707b9f597f597d
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2026-01-31 20:10:07 +0800

    rev. publish method

--- publish.cmd
+++ publish.cmd
@@ -1,2 +1,3 @@
 dotnet publish "D:\Github\SendAlerts\SendAlerts.Desktop\SendAlerts.Desktop.csproj" -c Release -r win-x64
 dotnet publish "D:\Github\SendAlerts\SendAlerts.Cli\SendAlerts.Cli.csproj" -c Release -r win-x64
+dotnet publish "D:\Github\SendAlerts\SendAlerts.Watchdog\SendAlerts.Watchdog.csproj" -c Release -r win-x64 -o "D:\Github\SendAlerts\SendAlerts.Desktop\bin\Release\net10.0\win-x64\publish"
